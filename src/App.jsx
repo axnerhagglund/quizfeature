76bbf4065b43b5599eff00fcf3ffd0fc52a56afb
axnerhagglund <axnerhagglund@gmail.com>
2025-08-14 10:51:24 +0200

    fixed project routing and so its ready to work on

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Layout from "./LayoutRouting/Layout"
+import QuizStartPage from "./QuizStartPage/QuizStartPage"
 function App() {
  
 
   return (
-    <>
-     hej
-    </>
+    <Router>
+     <Routes>
+        <Route path="/" element={<Layout/>}>
+            <Route path="/" element={<QuizStartPage/>}/>
+        </Route>
+     </Routes>
+    </Router>
   )
 }
 
---
+++ src/QuizStartPage/QuizStartPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function QuizStartPage() {
+  return (
+    <div>QuizStartPage</div>
+  )
+}
+
+export default QuizStartPage
